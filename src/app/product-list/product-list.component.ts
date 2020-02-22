a3bf1cd4b5f0065ed848b4d821afbd44fe797461
Angel Newton <xanewton@gmail.com>
2020-02-22 18:10:11 -0500

    Make the 'Notify Me' button work

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,11 +7,16 @@ import { products } from '../products';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
+
 export class ProductListComponent {
   products = products;
 
   share() {
     window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 }
 

578cee5e4c270d49c04eea75b4c464862efc3148
Angel Newton <xanewton@gmail.com>
2020-02-23 15:19:09 -0500

    Create the cart page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProductListComponent } from './product-list/product-list.component';
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
     ])
   ],
   declarations: [
